09859b31496b65e5ff2b73961c0bff9505281308
rohanmushan <rohanmushan854@gmail.com>
2025-12-20 12:06:25 +0530

    initial commit

---
+++ Even-Odd.py
@@ -0,0 +1,8 @@
+#Implement a program to check if a given number is even or odd using conditional operators
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
