81394a96b639d81d257b229f583be33abb5f83d4
rohanmushan <rohanmushan854@gmail.com>
2025-12-20 12:08:44 +0530

    initial commit

---
+++ printname.py
@@ -0,0 +1,6 @@
+#Implement a program that asks the user for their name and greets them.
+
+def greeting(name):
+    print(f"Hello, {name}! Nice to meet you.")
+name = input("Please enter your name: ")
+greeting(name)
