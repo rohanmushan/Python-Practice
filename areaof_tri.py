17ff4089e31d07c267d4d4ff190c38de99b25eeb
rohanmushan <rohanmushan854@gmail.com>
2025-12-21 10:46:17 +0530

    intitial commit

---
+++ areaof_tri.py
@@ -0,0 +1,7 @@
+#Write a program to find the area of a triangle given its base and height.
+base = float(input("Enter the length of the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+area = 0.5 * base * height
+
+print(f"The area of the triangle with base {base} and height {height} is {area}")
