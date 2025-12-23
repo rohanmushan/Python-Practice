98e2a667fc1809e0f54e3955ce71098605548124
rohanmushan <rohanmushan854@gmail.com>
2025-12-23 12:10:14 +0530

    initial commit

---
+++ 1-100/3_productoftwo.py
@@ -0,0 +1,16 @@
+#Calculate the product of two numbers entered by the user.
+
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+product=num1*num2
+print(f"The product of {num1} and {num2} is: {product}")
+
+
+#with function
+def product(num1, num2):
+    prod = num1 * num2
+    print(f"The product of {num1} and {num2} is {prod}")
+
+num1 = float(input("Enter 1st number: "))
+num2 = float(input("Enter 2nd number: "))
+product(num1, num2)
