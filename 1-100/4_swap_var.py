f4ce361fdf73ba466e87e8788190bdf29495515a
rohanmushan <rohanmushan854@gmail.com>
2025-12-23 12:10:37 +0530

    initial commit

---
+++ 1-100/4_swap_var.py
@@ -0,0 +1,18 @@
+#Write a program to swap two variables.
+
+# var1 = input("Enter the value for the first variable: ")
+# var2 = input("Enter the value for the second variable: ")
+# print(f"Before swapping: var1 = {var1}, var2 = {var2}")
+# var1, var2= var2, var1
+# print(f"After swapping: var1 = {var1}, var2 = {var2}")
+
+
+#with function
+def swap(var1, var2):
+    print(f"before swapping: var1 = {var1}, var2 = {var2}")
+    var1, var2 = var2, var1
+    print(f"After swapping: var1 = {var1}, var2 = {var2}")
+
+var1 = input("Enter the value of first variable: ")
+var2 = input("Enter the value of second variable: ")
+swap(var1, var2)
