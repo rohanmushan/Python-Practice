18d2fd0aa239ad7f254df1f84f397ff4440bf1fd
rohanmushan <rohanmushan854@gmail.com>
2025-12-24 18:41:32 +0530

    initial commit

---
+++ 1-100/8_Perimeter_circle.py
@@ -0,0 +1,13 @@
+#Calculate the perimeter of a circle given its radius
+radius=float(input("Enter the radius of the circle: "))
+circumference=2*3.14*radius
+print(f"The circumference of the circle is: {circumference}")
+
+#with function
+
+def perimeter_circle(r):
+    circumference = 2 * 3.14 * r 
+    print(f"The perimeter of the circle with radius {r} is: {circumference}")
+
+r = float(input("Enter Radius of Circle: "))
+perimeter_circle(r)
