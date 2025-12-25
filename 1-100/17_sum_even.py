d5fd5c574198f0588f181258c06f5197e616f89f
rohanmushan <rohanmushan854@gmail.com>
2025-12-25 20:42:57 +0530

    initial commit

---
+++ 1-100/17_sum_even.py
@@ -0,0 +1,18 @@
+#Calculate the sum of all even numbers between 1 and N.
+N=int(input("Enter value of N: "))
+sum=0
+for i in range(2, N+1,2):
+    sum=sum+i
+print(f"The sum of even numbers from 1 to {N} is: {sum}")
+
+#with function
+
+def sum_even(num):
+    sum = 0
+    for i in range(2, num+1, 2):
+        sum=sum+i
+    print(f"The sum of even numbers from 1 to {num} is: {sum}")
+
+num = int(input("Enter value of N: "))
+sum_even(num)
+
