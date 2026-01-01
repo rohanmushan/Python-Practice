f122cd895bb5c848d17e3893cae22cc7003b8874
rohanmushan <rohanmushan854@gmail.com>
2026-01-01 21:03:17 +0530

    initial commit

---
+++ 1-100/35_tri_herons.py
@@ -0,0 +1,25 @@
+#Calculate the area of a triangle using Heron's formula.
+
+import math
+a = float(input("Enter the length of side 'a': "))
+b = float(input("Enter the length of side 'b': "))
+c = float(input("Enter the length of side 'c': "))
+
+s=(a+b+c)/2
+area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+print(f"area is {area}")
+tri_area=area(a, b, c)
+print(f"The area of the triangle is: {tri_area}")
+
+#with function
+def area(a, b, c):
+    """Calculates the area of a triangle using Heron's formula."""
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    print(f"The Area of the triangle is: {area}")
+    tri_area = area(a, b, c)
+    print(f"The area of triangle using Heron's formula is {tri_area}")
+a = float(input("Enter the length of side 'a': "))
+b = float(input("Enter the length of side 'b': ")) 
+c = float(input("Enter the length of side 'c': "))
+area(a, b, c)
